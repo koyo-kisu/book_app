975a9681e74a920b385bdfcb77f0018eb8964029
Koyo Kisu <koyokisu@gmail.com>
2019-08-13 23:14:16 +0900

    first commit

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/* get */
 Route::get('books', 'Api\BookController@index');
+
+/* post */
 Route::post('create', 'Api\BookController@create');
